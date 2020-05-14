7bebf7a52ae4352bb5f8e6cd8444a242f2126d06
Christoph Henrici <chhenrici@gmail.com>
2020-05-14 20:25:46 +0200

    Standardoutput immediately

--- src/main/jenkins/durabilityTestsDeclarative.groovy
+++ src/main/jenkins/durabilityTestsDeclarative.groovy
@@ -1,27 +1,24 @@
 pipeline {
     agent any
     stages {
-        stage('Getting some Testcode') { // for display purposes
-            steps {
-                echo "Getting some test code"
-                git branch: 'IT-35749-Discussions', url: 'https://github.com/apgsga-it/apg-jenkins-pipelines.git'
-            }
-        }
         stage('Executing Testcode') { // for display purposes
             steps {
                 echo "Executeing a script , which uses rabbitmq"
                 script {
-                    outp = sh(returnStdout: true, script: "src/test/ruby/blabblr.rb").trim()
+                    outp = sh(returnStdout: true, script: "${WORKSPACE}/src/test/ruby/blabblr.rb").trim()
                     println "Script output: $outp"
                 }
             }
         }
         stage('Waiting for some Input') {
             steps {
+                options {
+                    disableResume()
+                }
                 echo "Waiting for Input"
                 timeout(time: 10, unit: 'DAYS') {
                     script {
-                        recievedStatus = sh(returnStatus: true, script: "src/test/ruby/input.rb")
+                        recievedStatus = sh(returnStatus: true, script: "${WORKSPACE}/src/test/ruby/input.rb")
                         println "Input Script status: $recievedStatus"
                     }
                 }
@@ -29,10 +26,13 @@ pipeline {
         }
         stage('Waiting some more') {
             steps {
+                options {
+                    disableResume()
+                }
                 echo "Waiting for some more Input"
                 timeout(time: 10, unit: 'DAYS') {
                     script {
-                        recievedStatus = sh(returnStatus: true, script: "src/test/ruby/input.rb")
+                        recievedStatus = sh(returnStatus: true, script: "${WORKSPACE}/src/test/ruby/input.rb")
                         println "Input Script status: $recievedStatus"
                     }
                 }
