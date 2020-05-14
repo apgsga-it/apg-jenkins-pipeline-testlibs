135e8f8be55e169f1346502504ff296929a15c1c
Christoph Henrici <chhenrici@gmail.com>
2020-05-14 19:24:16 +0200

    Jenkins Declartive Durability Tests

---
+++ src/main/jenkins/durabilityTestsDeclarative.groovy
@@ -0,0 +1,47 @@
+pipeline {
+    agent any
+    stages {
+        stage('Getting some Testcode') { // for display purposes
+            steps {
+                echo "Getting some test code"
+                git branch: 'IT-35749-Discussions', url: 'https://github.com/apgsga-it/apg-jenkins-pipelines.git'
+            }
+        }
+        stage('Executing Testcode') { // for display purposes
+            steps {
+                echo "Executeing a script , which uses rabbitmq"
+                script {
+                    outp = sh(returnStdout: true, script: "src/test/ruby/blabblr.rb").trim()
+                    println "Script output: $outp"
+                }
+            }
+        }
+        stage('Waiting for some Input') {
+            steps {
+                echo "Waiting for Input"
+                timeout(time: 10, unit: 'DAYS') {
+                    script {
+                        recievedMsg = sh(returnStdout: true, script: "src/test/ruby/input.rb").trim()
+                        println "Input Script output: $recievedMsg"
+                    }
+                }
+            }
+        }
+        stage('Waiting some more') {
+            steps {
+                echo "Waiting for some more Input"
+                timeout(time: 10, unit: 'DAYS') {
+                    script {
+                        recievedMsg = sh(returnStdout: true, script: "src/test/ruby/input.rb").trim()
+                        println "Input Script output: $recievedMsg"
+                    }
+                }
+            }
+        }
+        stage('Finishing up') {
+            steps {
+                echo "Oki what else"
+            }
+        }
+    }
+}
