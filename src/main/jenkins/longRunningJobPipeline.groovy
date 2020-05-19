77d1fc6c18a848658b6ff10364e28270fafdb735
Christoph Henrici <chhenrici@gmail.com>
2020-05-19 15:37:42 +0200

    Exception Handling

--- src/main/jenkins/longRunningJobPipeline.groovy
+++ src/main/jenkins/longRunningJobPipeline.groovy
@@ -4,43 +4,46 @@ pipeline {
         stage('Executing Long Running State') { // for display purposes
             steps {
                 echo "checkout apg-gradle-plugins-testsmodules von HEAD"
-                try {
-                    sh "cvs -d${env.CVS_ROOT} co apg-gradle-plugins-testsmodules"
-                } catch (err) {
-                    echo "Caught: ${err}"
-                    currentBuild.result = 'FAILURE'
-                    error("Cvs checkout faile: ${err}")
-                }
-                echo "Build Bom"
-                dir ("apg-gradle-plugins-testsmodules/testapp/testapp-bom") {
-                    withMaven( maven: 'apache-maven-3.5.0') { sh "mvn clean install" }
-                }
-                echo "Build Parent Pom"
-                dir ("apg-gradle-plugins-testsmodules/testapp/testapp/testapp-parentpom") {
-                    withMaven( maven: 'apache-maven-3.5.0') { sh "mvn clean install" }
-                }
-                echo "Build All"
-                dir ("apg-gradle-plugins-testsmodules/testapp/testapp") {
-                    withMaven( maven: 'apache-maven-3.5.0') { sh "mvn clean install" }
-                }
-                echo "Build Done"
-            }
-        }
-        stage('Waiting for some Input') {
-            steps {
-                echo "Waiting for Input"
-                timeout(time: 10, unit: 'DAYS') {
-                    script {
-                        recievedStatus = sh(returnStatus: true, script: "${WORKSPACE}/src/test/ruby/input.rb")
-                        println "Input Script status: $recievedStatus"
+                script {
+                    try {
+                        sh "cvs -d${env.CVS_ROOT} co apg-gradle-plugins-testsmodules"
+                    } catch (err) {
+                        echo "Caught: ${err}"
+                        currentBuild.result = 'FAILURE'
+                        error("Cvs checkout faile: ${err}")
                     }
                 }
             }
+            echo "Build Bom"
+            dir("apg-gradle-plugins-testsmodules/testapp/testapp-bom") {
+                withMaven(maven: 'apache-maven-3.5.0') { sh "mvn clean install" }
+            }
+            echo "Build Parent Pom"
+            dir("apg-gradle-plugins-testsmodules/testapp/testapp/testapp-parentpom") {
+                withMaven(maven: 'apache-maven-3.5.0') { sh "mvn clean install" }
+            }
+            echo "Build All"
+            dir("apg-gradle-plugins-testsmodules/testapp/testapp") {
+                withMaven(maven: 'apache-maven-3.5.0') { sh "mvn clean install" }
+            }
+            echo "Build Done"
         }
-        stage('Finishing up') {
-            steps {
-                echo "Oki what else"
+    }
+    stage('Waiting for some Input') {
+        steps {
+            echo "Waiting for Input"
+            timeout(time: 10, unit: 'DAYS') {
+                script {
+                    recievedStatus = sh(returnStatus: true, script: "${WORKSPACE}/src/test/ruby/input.rb")
+                    println "Input Script status: $recievedStatus"
+                }
             }
         }
     }
+    stage('Finishing up') {
+        steps {
+            echo "Oki what else"
+        }
+    }
+}
 }
