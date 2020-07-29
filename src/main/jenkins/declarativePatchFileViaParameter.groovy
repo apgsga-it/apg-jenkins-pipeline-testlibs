6632632905c022526f4013dd8d73d833442454e0
Christoph Henrici <chhenrici@gmail.com>
2020-07-29 14:33:24 +0200

    Update declarativePatchFileViaParameter.groovy

--- src/main/jenkins/declarativePatchFileViaParameter.groovy
+++ src/main/jenkins/declarativePatchFileViaParameter.groovy
@@ -7,6 +7,7 @@ def targetSystemMap = functions.loadTargetsMap(targetSystemMappingFile)
 println(targetSystemMap)
 node {
     def file_in_workspace = unstashFileParameter "patchFile.json"
+    fileOperations([fileDeleteOperation(includes: 'PatchFile.json'])
     fileOperations([fileRenameOperation(source: "${file_in_workspace}",  destination: 'PatchFile.json')])
     sh "cat PatchFile.json"
     stash name: "PatchFile" , includes:  'PatchFile.json'
