f008dfb41f8d3251bb9594b11cc6878b92605f60
Christoph Henrici <chhenrici@gmail.com>
2020-10-06 09:06:43 +0200

    Array / Restart Testcase

--- src/main/jenkins/declarativeReatartWithArray.groovy
+++ src/main/jenkins/declarativeReatartWithArray.groovy
@@ -4,6 +4,9 @@ def stageMappings = [ Entwickling:'CHEI212', Integration:'CHTI211']
 def stages = stageMappings.keySet() as List
 println(stageMappings)
 println(stages)
+node {
+    println "Do something"
+}
 pipeline {
     options {
         preserveStashes(buildCount: 2)
