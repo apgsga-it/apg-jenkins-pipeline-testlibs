ef7df7df83c363c9a18e0bc862904c793baaf344
Christoph Henrici <chhenrici@gmail.com>
2020-10-06 09:14:20 +0200

    Array / Restart Testcase

--- src/main/jenkins/declarativeReatartWithArray.groovy
+++ src/main/jenkins/declarativeReatartWithArray.groovy
@@ -2,28 +2,18 @@
 library 'testlib-functions'
 def stageMappings = [ Entwickling:'CHEI212', Integration:'CHTI211']
 def stages = stageMappings.keySet() as List
-println(stageMappings)
-println(stages)
-node {
-    println "Do something"
-}
 pipeline {
     options {
         preserveStashes(buildCount: 2)
-        timestamps()
+        timestamps ()
     }
     agent any
     stages {
-        stage('Init') {
+        stage(Entwicklung) {
             steps {
+                echo "Building for Target: ${targetSystemMap[Entwicklung]}"
                 script {
-//                    stageMappings.each { target ->
-//                        stage("Approve ${target} Build") {
-//                            functions.doSomething(stageMappings(target))
-//                        }
-//
-//                    }
-                    println "Do something"
+                    functions.approve()
                 }
             }
         }
